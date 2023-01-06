d8260bd086b758137951a74bc32303e16c95b0e8
JoaoCarvalhovs <120538480+JoaoCarvalhovs@users.noreply.github.com>
2023-01-06 16:00:25 -0400

    Add files via upload

--- 20230105001.c
+++ 20230105001.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-int intervalo(int xy){
+int scanIntIntervalo(int xy){
     while (xy < 0 || xy > 1000){
         printf("selecone um valor valido entre 0 e 1000\ntente novamente\ndefina o valor da variavel: ");
         scanf("%d", &xy);
@@ -14,13 +14,13 @@ int menor(int num1,int num2){
     return num1;
 }
 
-float porcentagem(float valor, float total){
+float percentual(float valor, float total){
     float result;
     result = (100 * valor)/total;
     return result;
 }
 
-float diferenca(float num1,float num2){
+float absdif(float num1,float num2){
     float result;
     result = num1 - num2;
     return result;
@@ -28,15 +28,16 @@ float diferenca(float num1,float num2){
 int main(){
     int x, y, intv, meno;
     float pctg, dif;
+
     printf("qual o valor de x: ");
     scanf("%d", &x);
-    intv = intervalo(x);
+    x = scanIntIntervalo(x);
     printf("qual o valor de y: ");
     scanf("%d", &y);
-    intv = intervalo(y);
+    y = scanIntIntervalo(y);
     meno = menor(x,y);
-    pctg = porcentagem(meno,x+y);
-    dif = diferenca(x,y);
+    pctg = percentual(meno,x+y);
+    dif = absdif(x,y);
     if (dif<0){
         dif *= -1;
     }
